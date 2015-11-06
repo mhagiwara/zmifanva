98c64c61d5c5753252edd436e6f9e32de8bedf34
Masato Hagiwara <masato@duolingo.com>
2015-11-05 22:59:56 -0500

    Added MERT training

--- scripts/train.sh
+++ scripts/train.sh
@@ -2,6 +2,7 @@
 
 rm -rf corpus
 rm -rf train
+rm -rf mert-work
 
 mkdir corpus
 
@@ -21,6 +22,13 @@ cat corpus/train | cut -f 2 | mosesdecoder/scripts/tokenizer/tokenizer.perl -l e
 
 mosesdecoder/scripts/training/clean-corpus-n.perl corpus/train.tok jb en corpus/train.clean 1 80
 
+# Create dev set
+
+python scripts/convert_solr_xml_to_bitext.py docs/crashcourse4.xml | python scripts/mod.py --mod 2 -n 1 > corpus/dev
+
+cat corpus/dev | cut -f 1 | python scripts/tokenize_jbo.py > corpus/dev.tok.jb
+cat corpus/dev | cut -f 2 | mosesdecoder/scripts/tokenizer/tokenizer.perl -l en > corpus/dev.tok.en
+
 # Build Lojban LM
 
 mosesdecoder/bin/lmplz -o 3 < corpus/train.clean.jb > corpus/train.arpa.jb
@@ -39,6 +47,13 @@ mosesdecoder/bin/build_binary corpus/train.arpa.en corpus/train.blm.en
 #     -lm 0:3:$PWD/corpus/train.blm.en:8 \
 #     -external-bin-dir mosesdecoder/tools
 
+# Tuning
+
+# mosesdecoder/scripts/training/mert-moses.pl \
+#     corpus/dev.tok.jb corpus/dev.tok.en \
+#     mosesdecoder/bin/moses train/model/moses.ini --mertdir $PWD/mosesdecoder/bin/ \
+#     --decoder-flags="-threads 4"
+
 # Build eng -> jbo model
 
 mosesdecoder/scripts/training/train-model.perl \
@@ -47,6 +62,11 @@ mosesdecoder/scripts/training/train-model.perl \
     -lm 0:3:$PWD/corpus/train.blm.jb:8 \
     -external-bin-dir mosesdecoder/tools
 
+
+mosesdecoder/scripts/training/mert-moses.pl \
+    corpus/dev.tok.en corpus/dev.tok.jb \
+    mosesdecoder/bin/moses train/model/moses.ini --mertdir $PWD/mosesdecoder/bin/ \
+    --decoder-flags="-threads 4"
 
 # Run moses in interative mode
 # mosesdecoder/bin/moses -f train/model/moses.ini
@@ -58,13 +78,14 @@ python scripts/convert_solr_xml_to_bitext.py docs/crashcourse4.xml | python scri
 cat corpus/test | cut -f 1 | python scripts/tokenize_jbo.py > corpus/test.tok.jb
 cat corpus/test | cut -f 2 | mosesdecoder/scripts/tokenizer/tokenizer.perl -l en > corpus/test.tok.en
 
-# mosesdecoder/bin/moses -f train/model/moses.ini < corpus/test.tok.jb > train/test.translated.en
+# mosesdecoder/bin/moses -f mert-work/moses.ini < corpus/test.tok.jb > train/test.translated.en
 # mosesdecoder/scripts/generic/multi-bleu.perl -lc corpus/test.tok.en < train/test.translated.en
 # BLEU = 21.80, 59.0/32.1/16.7/7.7 (BP=0.981, ratio=0.981, hyp_len=461, ref_len=470)
+# BLEU = 24.17, 60.6/32.3/17.4/10.4 (BP=0.989, ratio=0.989, hyp_len=465, ref_len=470) (after tuning)
 
 # Evaluate (eng -> jbo)
 
 mosesdecoder/bin/moses -f train/model/moses.ini < corpus/test.tok.en > train/test.translated.jb
 mosesdecoder/scripts/generic/multi-bleu.perl -lc corpus/test.tok.jb < train/test.translated.jb
 # BLEU = 27.69, 59.2/34.5/21.6/13.3 (BP=1.000, ratio=1.011, hyp_len=446, ref_len=441)
-
+# BLEU = 27.69, 59.2/34.5/21.6/13.3 (BP=1.000, ratio=1.011, hyp_len=446, ref_len=441) (after tuning)
