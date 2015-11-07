f5ba72215133538af1e04e45b610691217bdca06
Masato Hagiwara <masato@duolingo.com>
2015-11-06 22:40:25 -0500

    Add Lojban Crash Course Lesson 1 data -> for training

--- scripts/train.sh
+++ scripts/train.sh
@@ -14,6 +14,7 @@ python scripts/convert_solr_xml_to_bitext.py docs/phrasebook.xml >> corpus/train
 python scripts/convert_solr_xml_to_bitext.py docs/tatoeba.xml >> corpus/train
 python scripts/convert_solr_xml_to_bitext.py docs/teris.xml >> corpus/train
 python scripts/convert_solr_xml_to_bitext.py docs/introduction.xml >> corpus/train
+python scripts/convert_solr_xml_to_bitext.py docs/crashcourse1.xml >> corpus/train
 
 # Preprocess corpus
 
@@ -62,11 +63,11 @@ mosesdecoder/scripts/training/train-model.perl \
     -lm 0:3:$PWD/corpus/train.blm.jb:8 \
     -external-bin-dir mosesdecoder/tools
 
-
-mosesdecoder/scripts/training/mert-moses.pl \
-    corpus/dev.tok.en corpus/dev.tok.jb \
-    mosesdecoder/bin/moses train/model/moses.ini --mertdir $PWD/mosesdecoder/bin/ \
-    --decoder-flags="-threads 4"
+#
+# mosesdecoder/scripts/training/mert-moses.pl \
+#     corpus/dev.tok.en corpus/dev.tok.jb \
+#     mosesdecoder/bin/moses train/model/moses.ini --mertdir $PWD/mosesdecoder/bin/ \
+#     --decoder-flags="-threads 4"
 
 # Run moses in interative mode
 # mosesdecoder/bin/moses -f train/model/moses.ini
@@ -78,14 +79,27 @@ python scripts/convert_solr_xml_to_bitext.py docs/crashcourse4.xml | python scri
 cat corpus/test | cut -f 1 | python scripts/tokenize_jbo.py > corpus/test.tok.jb
 cat corpus/test | cut -f 2 | mosesdecoder/scripts/tokenizer/tokenizer.perl -l en > corpus/test.tok.en
 
-# mosesdecoder/bin/moses -f mert-work/moses.ini < corpus/test.tok.jb > train/test.translated.en
+# mosesdecoder/bin/moses -f train/model/moses.ini < corpus/test.tok.jb > train/test.translated.en
 # mosesdecoder/scripts/generic/multi-bleu.perl -lc corpus/test.tok.en < train/test.translated.en
+
+# Results
+# (no tuning)
 # BLEU = 21.80, 59.0/32.1/16.7/7.7 (BP=0.981, ratio=0.981, hyp_len=461, ref_len=470)
-# BLEU = 24.17, 60.6/32.3/17.4/10.4 (BP=0.989, ratio=0.989, hyp_len=465, ref_len=470) (after tuning)
+# (2015/11/05 ran MERT tuning)
+# BLEU = 24.17, 60.6/32.3/17.4/10.4 (BP=0.989, ratio=0.989, hyp_len=465, ref_len=470)
+# (2015/11/06 added crashcourse1, no tuning)
+# BLEU = 21.26, 58.6/31.0/16.2/8.1 (BP=0.961, ratio=0.962, hyp_len=452, ref_len=470)
 
 # Evaluate (eng -> jbo)
 
 mosesdecoder/bin/moses -f train/model/moses.ini < corpus/test.tok.en > train/test.translated.jb
 mosesdecoder/scripts/generic/multi-bleu.perl -lc corpus/test.tok.jb < train/test.translated.jb
+
+# Results
+# (no tuning)
 # BLEU = 27.69, 59.2/34.5/21.6/13.3 (BP=1.000, ratio=1.011, hyp_len=446, ref_len=441)
-# BLEU = 27.69, 59.2/34.5/21.6/13.3 (BP=1.000, ratio=1.011, hyp_len=446, ref_len=441) (after tuning)
+# (2015/11/05 ran MERT tuning
+# BLEU = 28.06, 61.2/35.9/22.9/15.0 (BP=0.951, ratio=0.952, hyp_len=420, ref_len=441)
+# (2015/11/06 added crashcourse1, no tuning)
+# BLEU = 27.64, 60.7/35.6/21.7/13.2 (BP=0.986, ratio=0.986, hyp_len=435, ref_len=441)
+
